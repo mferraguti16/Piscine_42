adbfad2095d47acedd6cd571da22c244ef8234d0
mferraguti16 <mathilde.ferraguti@hotmail.fr>
2025-01-15 01:26:35 +0100

    test:main_ft_strncpy

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -10,11 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	i;
+	unsigned int	i; // unsigned int == positive or null, never negative 
 
 	i = 0;
 	while (src[i] != '\0' && i < n)
@@ -30,26 +28,40 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-/* #include <stdio.h>
-
+#include <stdio.h>
 int main (void)
 {
-        char src[]= "SEGFAULT";
-        char dest[9];
-        unsigned int n = 5;
-        printf("%s",ft_strncpy(dest, src, n));
-        return 0;
+	char src[]= "SEGFAULT";
+    char dest[6]; // argc
+    unsigned int n = 5; // SEGFA\0 = 6
+	int i = 0;
+
+	ft_strncpy(dest, src, n);
+
+	// print strings
+	printf("src is : %s\n", src);
+	printf("dest is : %s\n", dest);
+
+	// check position last int n but with %c type char data cause clearer
+	while (i < 5)
+	{
+		printf("Value at dest[%d] : %c\n", i, dest[i]);
+		i++;
+	}
+	// check position null byte with %d calls ASCII value 0 cause not printable cause control char (blank space with %c)
+	printf("Valeur à dest[5]: %d\n", dest[5]); 
+
+	if (dest[5] == '\0') // array begins at 0 so dest[5] for checking null byte not 10!
+    	printf("Le caractère nul \\0 est bien copié à la position 5.\n");
+    else
+        printf("Le caractère nul \\0 n'est pas présent ou mal positionné.\n");
+
+	return 0;
 }
 
-The strcpy() function copies the string pointed
-to by src, including the terminating null byte ('\0'), 
-to the buffer pointed to by dest.
-The strings may not overlap, and the destination 
-string dest must be large enough to receive the copy.
-Beware of  buffer overruns!  (See BUGS.)
-The  strncpy()  function  is similar, 
+/* strncpy() is similar at strcpy, 
 except that at most n bytes of src are copied.  
 Warning: If there is no null byte among the
 first n bytes of src, the string placed 
 in dest will not be null-terminated.
-unsigned int = positive or null, never negative */
+We have to add it manually */
