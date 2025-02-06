ac0dc6b16afe879264ba541121a01424aef79c36
mferraguti16 <mathilde.ferraguti@hotmail.fr>
2025-02-06 18:46:06 +0100

    fix:add_all_main_pull

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mferragu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mathilfe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 21:43:47 by mferragu          #+#    #+#             */
-/*   Updated: 2022/08/28 15:26:26 by mferragu         ###   ########.fr       */
+/*   Created: 2023/07/03 10:02:32 by mathilfe          #+#    #+#             */
+/*   Updated: 2023/07/10 09:26:17 by mathilfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (str[i] != '\0')
+	{
 		write(1, &str[i++], 1);
+	}
+}
+
+int     main(int argc, char **argv)
+{
+    if (argc > 1)
+	{
+        ft_putstr(argv[1]);
+	}
+	write(1, "\n", 1);
+    return (0);
 }
